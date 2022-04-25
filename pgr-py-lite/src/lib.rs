7ca21dc0a4af8d94a6fce3f676464908e19335ea
Jason Chin <cschin@infoecho.net>
2022-04-25 17:08:43 +0000

    format / maintaince

--- pgr-py-lite/src/lib.rs
+++ pgr-py-lite/src/lib.rs
@@ -1,12 +1,12 @@
 // src/lib.rs
+use pgr_db::agc_io;
+use pgr_db::seq_db::{query_fragment, read_shmr_map_file, FragmentSignature, ShmmrToFrags};
+use pgr_utils::fasta_io;
 use pyo3::exceptions;
 use pyo3::prelude::*;
 use pyo3::types::PyString;
 use pyo3::wrap_pyfunction;
 use pyo3::Python;
-use pgr_db::agc_io;
-use pgr_db::seq_db::{query_fragment, read_shmr_map_file, FragmentSignature, ShmmrToFrags};
-use pgr_utils::fasta_io;
 use rustc_hash::FxHashMap;
 
 #[pyclass]
@@ -14,7 +14,6 @@ use rustc_hash::FxHashMap;
 struct ShmmrFragMap {
     pub shmmr_to_frags: ShmmrToFrags,
 }
-
 
 #[pymethods]
 impl ShmmrFragMap {
@@ -50,12 +49,13 @@ struct AGCFile {
 impl AGCFile {
     #[new]
     pub fn new(filepath: String) -> Self {
-        let agc_file = agc_io::AGCFile::new(filepath); 
-        let mut ctg_lens = FxHashMap::<(String, String), usize>::default(); 
-        agc_file.ctg_lens.iter().for_each(|(k,v)| {ctg_lens.insert((k.0.clone(), k.1.clone()),*v);}); 
-        AGCFile{agc_file, ctg_lens}
+        let agc_file = agc_io::AGCFile::new(filepath);
+        let mut ctg_lens = FxHashMap::<(String, String), usize>::default();
+        agc_file.ctg_lens.iter().for_each(|(k, v)| {
+            ctg_lens.insert((k.0.clone(), k.1.clone()), *v);
+        });
+        AGCFile { agc_file, ctg_lens }
     }
-
 
     pub fn get_sub_seq(
         &self,
@@ -65,15 +65,12 @@ impl AGCFile {
         end: usize,
     ) -> PyResult<Vec<u8>> {
         Ok(self.agc_file.get_sub_seq(sample_name, ctg_name, bgn, end))
-    } 
+    }
 
     pub fn get_seq(&self, sample_name: String, ctg_name: String) -> PyResult<Vec<u8>> {
         Ok(self.agc_file.get_seq(sample_name, ctg_name))
     }
 }
-
-
-
 
 #[pymodule]
 fn pgrlite(_: Python, m: &PyModule) -> PyResult<()> {
