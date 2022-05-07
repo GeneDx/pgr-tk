3e8b445315daaff40d00a2751bf9fd93837af605
Jason Chin <cschin@infoecho.net>
2022-05-07 16:37:01 +0000

    test github action build

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ rm -rf WFA
 git clone https://github.com/cschin/WFA.git --depth=1
 popd
 
+git submodule update --init
 rustup default stable
 cargo build -p libwfa --release
 cargo build -p pgr-db --release
