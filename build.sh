b47b8e8977faf5232c3b9e4b4b6e95144a99d9c5
Jason Chin <cschin@infoecho.net>
2022-05-07 16:40:02 +0000

    test github action build

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
+
+git submodule update --init
+
 pushd rs-wfa/
 rm -rf WFA
 git clone https://github.com/cschin/WFA.git --depth=1
 popd
 
-git submodule update --init
 rustup default stable
 cargo build -p libwfa --release
 cargo build -p pgr-db --release
 
-#pushd pgr-py-lite/
-#maturin build --release
-#maturin build --release --skip-auditwheel
-#popd
+pushd pgr-py-lite/
+maturin build --release
+maturin build --release --skip-auditwheel
+popd
