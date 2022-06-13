bc5dc584c9281d5a2c73b368c6241be5d51ba82d
Jason Chin <cschin@infoecho.net>
2022-06-13 03:35:00 +0000

    add shmmr_dbg_consensus()

--- pgr-db/src/ec.rs
+++ pgr-db/src/ec.rs
@@ -1,8 +1,13 @@
 #![warn(missing_docs)]
 //! function for error correction
 
+use crate::fasta_io::reverse_complement;
+use crate::graph_utils::ShmmrGraphNode;
+use crate::seq_db::pair_shmmrs;
+use crate::shmmrutils::{sequence_to_shmmrs, ShmmrSpec};
 use petgraph::algo::toposort;
 use petgraph::{graphmap::DiGraphMap, EdgeDirection::Incoming};
+use rayon::prelude::*;
 use rustc_hash::FxHashMap;
 
 /// perform error correction using de Bruijn graph
@@ -148,9 +153,144 @@ pub fn naive_dbg_consensus(
     }
 }
 
+/// perform error correction using shimmer de Bruijn graph
+///
+/// this methods can ignore haplotype specific signals
+///
+pub fn shmmr_dbg_consensus(
+    seqs: Vec<Vec<u8>>,
+    shmmr_spec: &Option<ShmmrSpec>,
+) -> Result<Vec<u8>, &'static str> {
+    let shmmr_spec = shmmr_spec.as_ref().unwrap_or(&ShmmrSpec {
+        w: 1,
+        k: 32,
+        r: 1,
+        min_span: 0,
+        sketch: false,
+    });
+
+    let pairs = seqs
+        .par_iter()
+        .enumerate()
+        .flat_map(|(sid, seq)| {
+            //println!("len {}", seq.len());
+            let shmmrs = sequence_to_shmmrs(sid as u32, seq, shmmr_spec, true);
+
+            let pairs = pair_shmmrs(&shmmrs)
+                .iter()
+                .map(|(&shmmr0, &shmmr1)| {
+                    let s0 = shmmr0.x >> 8;
+                    let s1 = shmmr1.x >> 8;
+                    let p0 = ((shmmr0.y & 0xFFFF_FFFF) >> 1) as u32;
+                    let p1 = ((shmmr1.y & 0xFFFF_FFFF) >> 1) as u32;
+                    let shmmr_pair = if s0 <= s1 {
+                        (s0, s1, 0_u8, sid as u32, p0, p1)
+                    } else {
+                        (s1, s0, 1_u8, sid as u32, p0, p1)
+                    };
+                    shmmr_pair
+                })
+                .collect::<Vec<(u64, u64, u8, u32, u32, u32)>>();
+            pairs
+        })
+        .collect::<Vec<(u64, u64, u8, u32, u32, u32)>>();
+
+    let mut frg_seqs = FxHashMap::<(u64, u64, u8), Vec<u8>>::default();
+    pairs.iter().for_each(|&v| {
+        let (s0, s1, strand, sid, p0, p1) = v;
+        let b = (p0 - shmmr_spec.k) as usize;
+        let e = (p1 + 1) as usize;
+        let seq = seqs[sid as usize][b..e].to_vec();
+        if !frg_seqs.contains_key(&(s0, s1, strand)) {
+            frg_seqs.insert((s0, s1, strand), seq.clone());
+        };
+        let seq = reverse_complement(&seq);
+        if !frg_seqs.contains_key(&(s0, s1, 1 - strand)) {
+            frg_seqs.insert((s0, s1, 1 - strand), seq.clone());
+        };
+    });
+
+    let adj_list = (0..pairs.len() - 1)
+        .into_par_iter()
+        .flat_map(|i| {
+            let v = pairs[i];
+            let w = pairs[i + 1];
+            vec![
+                (v.3 as u32, (v.0, v.1, v.2), (w.0, w.1, w.2)),
+                (v.3 as u32, (w.0, w.1, 1 - w.2), (v.0, v.1, 1 - v.2)),
+            ]
+        })
+        .collect::<Vec<(u32, (u64, u64, u8), (u64, u64, u8))>>();
+
+    //adj_list.iter().for_each(|f|println!("{:?}", f));
+
+    let s0 = adj_list[0];
+
+    println!("s0:{:?}", s0);
+
+    let start = ShmmrGraphNode(s0.1 .0, s0.1 .1, s0.1 .2);
+
+    use crate::graph_utils::BiDiGraphWeightedDfs;
+
+    let mut g = DiGraphMap::<ShmmrGraphNode, ()>::new();
+    let mut score = FxHashMap::<ShmmrGraphNode, u32>::default();
+    adj_list.into_iter().for_each(|(_sid, v, w)| {
+        let v = ShmmrGraphNode(v.0, v.1, v.2);
+        let w = ShmmrGraphNode(w.0, w.1, w.2);
+        g.add_edge(v, w, ());
+
+        //println!("DBG: add_edge {:?} {:?}", v, w);
+        *score.entry(v).or_insert(0) += 1;
+        *score.entry(w).or_insert(0) += 1;
+    });
+
+    println!("DBG: node_count {:?} {:?}", g.node_count(), g.edge_count());
+    //println!("DBG: {} {}", g.node_count(), g.edge_count());
+
+    let mut wdfs_walker = BiDiGraphWeightedDfs::new(&g, start, &score);
+    let mut out = vec![];
+    loop {
+        if let Some((node, p_node, is_leaf, rank, branch_id, branch_rank)) = wdfs_walker.next(&g) {
+            let node_count = *score.get(&node).unwrap();
+            let p_node = match p_node {
+                Some(pnode) => Some((pnode.0, pnode.1, pnode.2)),
+                None => None,
+            };
+            out.push((
+                (node.0, node.1, node.2),
+                p_node,
+                node_count,
+                is_leaf,
+                rank,
+                branch_id,
+                branch_rank,
+            ));
+        } else {
+            break;
+        }
+    }
+
+    let mut out_seq = vec![];
+    let n = out[0];
+    let seq = frg_seqs.get(&n.0).unwrap().clone();
+
+    out_seq.extend(seq[..1+shmmr_spec.k as usize].to_vec()); 
+    for (node, _p_node, _node_count, is_leaf, _rank, _branch_id, _branch_rank) in out {
+        out_seq.extend(
+            frg_seqs.get(&(node.0, node.1, node.2)).unwrap()[1 + shmmr_spec.k as usize..].to_vec(),
+        );
+        if is_leaf {
+            break;
+        }
+    }
+
+    Ok(out_seq)
+}
+
 #[cfg(test)]
 mod test {
     use crate::ec::naive_dbg_consensus;
+    use crate::ec::shmmr_dbg_consensus;
     use crate::seq_db::CompactSeqDB;
     use crate::shmmrutils::ShmmrSpec;
     #[test]
@@ -170,6 +310,26 @@ mod test {
             .collect::<Vec<Vec<u8>>>();
 
         let r = naive_dbg_consensus(seqs, 48, 2).unwrap();
+        println!("{}", String::from_utf8_lossy(&r[..]));
+    }
+
+    #[test]
+    fn test_shmmr_dbg_consensus() {
+        let spec = ShmmrSpec {
+            w: 24,
+            k: 24,
+            r: 12,
+            min_span: 12,
+            sketch: false,
+        };
+        let mut sdb = CompactSeqDB::new(spec);
+        let _ = sdb.load_seqs_from_fastx("test/test_data/consensus_test.fa".to_string());
+        let seqs = (0..sdb.seqs.len())
+            .into_iter()
+            .map(|sid| sdb.get_seq_by_id(sid as u32))
+            .collect::<Vec<Vec<u8>>>();
+
+        let r = shmmr_dbg_consensus(seqs, &None).unwrap();
         println!("{}", String::from_utf8_lossy(&r[..]));
     }
 }
