d256c5f5a0fcb549f026404e9c3037e4bf8f1e43
Jason Chin <cschin@infoecho.net>
2022-07-28 20:03:13 +0000

    bump verison/maintainance

--- pgr-tk-workstation/build.sh
+++ pgr-tk-workstation/build.sh
@@ -1,2 +1,3 @@
-cp ../target/wheels/pgrtk-0.3.2-cp38-cp38-linux_x86_64.whl .
-docker build -t pgr-tk-ws:dev .
+cp ../target/wheels/pgrtk-0.3.3-cp38-cp38-linux_x86_64.whl .
+#docker build -t cschin/pgr-tk-ws:v0.3.3_dev .
+docker build -t cschin/pgr-tk-ws:v0.3.3 .
--- pgr-tk-workstation/jupyterlab.sh
+++ pgr-tk-workstation/jupyterlab.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-ln -s /wd/* /
+mkdir -p /wd/results/
+mkdir -p /wd/code/
+mkdir -p /wd/data//
+ln -sf /wd/* /
 . /opt/conda/etc/profile.d/conda.sh
 jupyter-lab --ip="*" --allow-root --no-browser --port 8899 --NotebookApp.disable_check_xsrf=True /wd
