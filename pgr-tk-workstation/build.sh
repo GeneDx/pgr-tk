eddda4954400ea769cee5acfee6df8d02e00bfb0
Jason Chin <cschin@infoecho.net>
2022-08-03 22:00:50 +0000

    update build to use v0.3.4 tag

--- pgr-tk-workstation/build.sh
+++ pgr-tk-workstation/build.sh
@@ -1,3 +1,3 @@
-cp ../target/wheels/pgrtk-0.3.3-cp38-cp38-linux_x86_64.whl .
+cp ../target/wheels/pgrtk-0.3.4-cp38-cp38-linux_x86_64.whl .
 #docker build -t cschin/pgr-tk-ws:v0.3.3_dev .
-docker build -t cschin/pgr-tk-ws:v0.3.3 .
+docker build -t cschin/pgr-tk-ws:v0.3.4 .
