c45fdf7904c7ee16f0e0bfb8ab4f16108c183c64
Jason Chin <cschin@infoecho.net>
2022-08-03 22:15:12 +0000

    use the default port

--- pgr-tk-workstation/jupyterlab.sh
+++ pgr-tk-workstation/jupyterlab.sh
@@ -4,4 +4,4 @@ mkdir -p /wd/code/
 mkdir -p /wd/data//
 ln -sf /wd/* /
 . /opt/conda/etc/profile.d/conda.sh
-jupyter-lab --ip="*" --allow-root --no-browser --port 8899 --NotebookApp.disable_check_xsrf=True /wd
+jupyter-lab --ip="*" --allow-root --no-browser --port 8888 --NotebookApp.disable_check_xsrf=True /wd
