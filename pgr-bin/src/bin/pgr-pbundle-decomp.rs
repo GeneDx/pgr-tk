2fc41a1b573b996e76c14454e2b5fc3920544e1c
Jason Chin <cschin@infoecho.net>
2022-12-22 08:21:07 +0000

    add two command line tools for generation principal bundle decomposition

---
+++ pgr-bin/src/bin/pgr-pbundle-decomp.rs
@@ -0,0 +1,174 @@
+const VERSION_STRING: &'static str = env!("VERSION_STRING");
+use clap::{self, CommandFactory, Parser};
+use pgr_bin::SeqIndexDB;
+use rustc_hash::FxHashMap;
+//use std::fs::File;
+use std::{fs::File, io::BufWriter, io::Write, path::Path};
+
+#[derive(Parser, Debug)]
+#[clap(name = "pgr-query")]
+#[clap(author, version)]
+#[clap(about = "take a fasta file and output the principal bundle decomposition though MAP Graph", long_about = None)]
+struct CmdOptions {
+    fastx_path: String,
+    output_prefix: String,
+    #[clap(short, default_value_t = 80)]
+    w: u32,
+    #[clap(short, default_value_t = 56)]
+    k: u32,
+    #[clap(short, default_value_t = 4)]
+    r: u32,
+    #[clap(long, default_value_t = 16)]
+    min_span: u32,
+    #[clap(long, default_value_t = 2)]
+    min_cov: usize,
+    #[clap(long, default_value_t = 8)]
+    min_branch_size: usize,
+    #[clap(long, default_value_t = 2500)]
+    bundle_length_cutoff: usize,
+    #[clap(long, default_value_t = 10000)]
+    bundle_merge_distance: usize,
+}
+
+fn group_smps_by_principle_bundle_id(
+    smps: &Vec<((u64, u64, u32, u32, u8), Option<(usize, u8, usize)>)>,
+    bundle_length_cutoff: usize,
+    bundle_merge_distance: usize,
+) -> Vec<Vec<((u64, u64, u32, u32, u8), usize, u32, usize)>> {
+    let mut pre_bundle_id: Option<usize> = None;
+    let mut pre_direction: Option<u32> = None;
+    let mut all_partitions = vec![];
+    let mut new_partition = vec![];
+    smps.into_iter().for_each(|&(smp, bundle_info)| {
+        if bundle_info.is_none() {
+            return;
+        };
+        let bundle_info = bundle_info.unwrap();
+        let d = if smp.4 == bundle_info.1 { 0_u32 } else { 1_u32 };
+        let bid = bundle_info.0;
+        let bpos = bundle_info.2;
+        if pre_bundle_id.is_none() {
+            new_partition.clear();
+            new_partition.push((smp, bid, d, bpos));
+            pre_bundle_id = Some(bid);
+            pre_direction = Some(d);
+            return;
+        };
+        if bid != pre_bundle_id.unwrap() || d != pre_direction.unwrap() {
+            let l = new_partition.len();
+            if new_partition[l - 1].0 .3 as usize - new_partition[0].0 .2 as usize
+                > bundle_length_cutoff
+            {
+                all_partitions.push(new_partition.clone());
+                new_partition.clear();
+            } else {
+                new_partition.clear();
+            };
+            pre_bundle_id = Some(bid);
+            pre_direction = Some(d);
+        };
+        new_partition.push((smp, bid, d, bpos));
+    });
+    let l = new_partition.len();
+    if new_partition[l - 1].0 .3 as usize - new_partition[0].0 .2 as usize > bundle_length_cutoff {
+        all_partitions.push(new_partition.clone());
+    };
+
+    let mut rtn_partitions = vec![];
+    let mut partition = all_partitions[0].clone();
+    (1..all_partitions.len()).into_iter().for_each(|idx| {
+        let p = all_partitions[idx].clone();
+        let p_len = partition.len();
+        let p_end = partition[p_len - 1].0 .3;
+        let p_bid = partition[p_len - 1].1;
+        let p_d = partition[p_len - 1].2;
+        let np_bgn = p[0].0 .2;
+        let np_bid = p[0].1;
+        let np_d = p[0].2;
+        if p_bid == np_bid
+            && p_d == np_d
+            && (np_bgn as i64 - p_end as i64).abs() < bundle_merge_distance as i64
+        {
+            partition.extend(p);
+        } else {
+            rtn_partitions.push(partition.clone());
+            partition = p.clone();
+        }
+    });
+    rtn_partitions
+}
+
+fn main() -> Result<(), std::io::Error> {
+    CmdOptions::command().version(VERSION_STRING).get_matches();
+    let args = CmdOptions::parse();
+    let mut seq_index_db = SeqIndexDB::new();
+    let _ = seq_index_db.load_from_fastx(args.fastx_path, args.w, args.k, args.r, args.min_span);
+    let (principal_bundles, sid_smps) =
+        seq_index_db.get_principal_bundle_decomposition(args.min_cov, args.min_branch_size, None);
+
+    let bid_to_size = principal_bundles
+        .iter()
+        .map(|v| (v.0, v.2.len()))
+        .collect::<FxHashMap<usize, usize>>();
+    let sid_smps: FxHashMap<u32, Vec<_>> = sid_smps.into_iter().collect();
+
+    let output_prefix_path = Path::new(&args.output_prefix);
+    seq_index_db.generate_mapg_gfa(
+        0,
+        output_prefix_path
+            .with_extension("mapg.gfa")
+            .to_str()
+            .unwrap(),
+        "from_fragmap",
+        None,
+    )?;
+    seq_index_db.write_mapg_idx(
+        output_prefix_path
+            .with_extension("mapg.idx")
+            .to_str()
+            .unwrap(),
+    )?;
+    seq_index_db.generate_principal_mapg_gfa(
+        args.min_cov,
+        args.min_branch_size,
+        output_prefix_path
+            .with_extension("pmapg.gfa")
+            .to_str()
+            .unwrap(),
+        None,
+    )?;
+    let mut outpu_bed_file =
+        BufWriter::new(File::create(output_prefix_path.with_extension("bed"))?);
+    seq_index_db
+        .seq_info
+        .unwrap()
+        .into_iter()
+        .for_each(|(sid, sdata)| {
+            let (ctg, _src, _len) = sdata;
+            let smps = sid_smps.get(&sid).unwrap();
+            let smp_partitions = group_smps_by_principle_bundle_id(
+                smps,
+                args.bundle_length_cutoff,
+                args.bundle_merge_distance,
+            );
+            smp_partitions.into_iter().for_each(|p| {
+                let b = p[0].0 .2;
+                let e = p[p.len() - 1].0 .3 + args.k;
+                let bid = p[0].1;
+                let direction = p[0].2;
+                let _ = writeln!(
+                    outpu_bed_file,
+                    "{}\t{}\t{}\t{}:{}:{}:{}:{}",
+                    ctg,
+                    b,
+                    e,
+                    bid,
+                    bid_to_size[&bid],
+                    direction,
+                    p[0].3,
+                    p[p.len() - 1].3
+                );
+            });
+        });
+    Ok(())
+}
---
+++ pgr-bin/src/bin/pgr-query.rs
@@ -0,0 +1,255 @@
+const VERSION_STRING: &'static str = env!("VERSION_STRING");
+use clap::{self, CommandFactory, Parser};
+use pgr_bin::{get_fastx_reader, GZFastaReader, SeqIndexDB};
+use pgr_db::fasta_io::SeqRec;
+use rustc_hash::FxHashMap;
+use std::fs::File;
+use std::io::{self, BufWriter, Write};
+use std::path::Path;
+
+#[derive(Parser, Debug)]
+#[clap(name = "pgr-query")]
+#[clap(author, version)]
+#[clap(about = "query a pgr pangenome database and ouput the hits", long_about = None)]
+struct CmdOptions {
+    pgr_db_prefix: String,
+    query_fastx_path: String,
+    output_prfix: String,
+
+    #[clap(long, short, default_value_t = 0.025)]
+    gap_penality_factor: f32,
+
+    #[clap(long, short, default_value_t = 100000)]
+    merge_range_tol: usize,
+
+    #[clap(long, default_value_t = 128)]
+    max_count: u32,
+
+    #[clap(long, default_value_t = 128)]
+    max_query_count: u32,
+
+    #[clap(long, default_value_t = 128)]
+    max_target_count: u32,
+
+    #[clap(long, default_value_t = 8)]
+    max_aln_chain_span: u32,
+}
+
+fn main() -> Result<(), std::io::Error> {
+    CmdOptions::command().version(VERSION_STRING).get_matches();
+    let args = CmdOptions::parse();
+
+    let mut query_seqs: Vec<SeqRec> = vec![];
+    let mut add_seqs = |seq_iter: &mut dyn Iterator<Item = io::Result<SeqRec>>| {
+        seq_iter.into_iter().for_each(|r| {
+            if let Ok(r) = r {
+                query_seqs.push(r);
+            };
+        });
+    };
+
+    match get_fastx_reader(args.query_fastx_path)? {
+        GZFastaReader::GZFile(reader) => add_seqs(&mut reader.into_iter()),
+
+        GZFastaReader::RegularFile(reader) => add_seqs(&mut reader.into_iter()),
+    };
+
+    let mut seq_index_db = SeqIndexDB::new();
+    let _ = seq_index_db.load_from_agc_index(args.pgr_db_prefix);
+    let prefix = Path::new(&args.output_prfix);
+    let mut hit_file = BufWriter::new(File::create(prefix.with_extension("hit")).unwrap());
+    writeln!(
+        hit_file,
+        "#{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
+        "idx",
+        "q_name",
+        "ctg",
+        "ctg_bgn",
+        "ctg_end",
+        "orientation",
+        "aln_anchor_count",
+        "query_bgn",
+        "query_end"
+    )?;
+
+    query_seqs
+        .into_iter()
+        .enumerate()
+        .for_each(|(idx, seq_rec)| {
+            let q_name = String::from_utf8_lossy(&seq_rec.id);
+            let query_seq = seq_rec.seq;
+
+            let query_results = seq_index_db.query_fragment_to_hps(
+                query_seq,
+                args.gap_penality_factor,
+                Some(args.max_count),
+                Some(args.max_query_count),
+                Some(args.max_target_count),
+                Some(args.max_aln_chain_span),
+            );
+
+            if let Some(qr) = query_results {
+                let mut sid_to_alns = FxHashMap::default();
+                qr.into_iter().for_each(|(sid, alns)| {
+                    let mut aln_lens = vec![];
+                    let mut f_count = 0_usize;
+                    let mut r_count = 0_usize;
+                    alns.into_iter().for_each(|(_score, aln)| {
+                        if aln.len() > 2 {
+                            aln_lens.push(aln.len());
+                            for hp in &aln {
+                                if hp.0 .2 == hp.1 .2 {
+                                    f_count += 1;
+                                } else {
+                                    r_count += 1;
+                                }
+                            }
+                            let orientation = if f_count > r_count { 0_u32 } else { 1_u32 };
+                            let e = sid_to_alns.entry(sid).or_insert(vec![]);
+                            e.push((aln, orientation))
+                        }
+                    })
+                });
+
+                let mut aln_range = FxHashMap::default();
+                sid_to_alns.into_iter().for_each(|(sid, alns)| {
+                    alns.into_iter().for_each(|(aln, orientation)| {
+                        let mut target_coordiantes = aln
+                            .iter()
+                            .map(|v| (v.1 .0, v.1 .1))
+                            .collect::<Vec<(u32, u32)>>();
+                        target_coordiantes.sort();
+                        let bgn = target_coordiantes[0].0;
+                        let end = target_coordiantes[target_coordiantes.len() - 1].1;
+                        let e = aln_range.entry(sid).or_insert(vec![]);
+                        e.push((bgn, end, end - bgn, orientation, aln));
+                    })
+                });
+
+                // TODO: merge aln_range
+                let aln_range = aln_range
+                    .into_iter()
+                    .map(|(sid, rgns)| {
+                        let mut f_rgns = rgns
+                            .iter()
+                            .filter(|&v| v.3 == 0)
+                            .map(|v| v.clone())
+                            .collect::<Vec<_>>();
+
+                        let mut r_rgns = rgns
+                            .iter()
+                            .filter(|&v| v.3 == 1)
+                            .map(|v| v.clone())
+                            .collect::<Vec<_>>();
+
+                        f_rgns.sort();
+                        r_rgns.sort();
+
+                        let mut out_rgns = vec![];
+                        let mut last_rgn: (u32, u32, u32, u32, Vec<_>) = (0, 0, 0, 0, vec![]);
+                        f_rgns.into_iter().for_each(|r| {
+                            if last_rgn.4.len() == 0 {
+                                last_rgn = r.clone();
+                                return;
+                            } else {
+                                let l_bgn = last_rgn.0;
+                                let l_end = last_rgn.1;
+                                assert!(l_end > l_bgn);
+                                let r_bgn = r.0;
+                                let r_end = r.1;
+                                if (r_bgn as i64) - (l_end as i64) < args.merge_range_tol as i64 {
+                                    let bgn = l_bgn;
+                                    let end = if r_end > l_end { r_end } else { l_end };
+                                    let len = end - bgn;
+                                    let orientation = last_rgn.3;
+                                    let mut aln = last_rgn.4.clone();
+                                    aln.extend(r.4);
+                                    last_rgn = (bgn, end, len, orientation, aln);
+                                } else {
+                                    out_rgns.push(last_rgn.clone());
+                                    last_rgn = r.clone();
+                                }
+                            }
+                        });
+                        if last_rgn.2 > 0 {
+                            //not empty
+                            out_rgns.push(last_rgn.clone());
+                        };
+
+                        let mut last_rgn: (u32, u32, u32, u32, Vec<_>) = (0, 0, 0, 0, vec![]);
+                        r_rgns.into_iter().for_each(|r| {
+                            if last_rgn.4.len() == 0 {
+                                last_rgn = r.clone();
+                                return;
+                            } else {
+                                let l_bgn = last_rgn.0;
+                                let l_end = last_rgn.1;
+                                assert!(l_end > l_bgn);
+                                let r_bgn = r.0;
+                                let r_end = r.1;
+                                if (r_bgn as i64) - (l_end as i64) < args.merge_range_tol as i64 {
+                                    let bgn = l_bgn;
+                                    let end = if r_end > l_end { r_end } else { l_end };
+                                    let len = end - bgn;
+                                    let orientation = last_rgn.3;
+                                    let mut aln = last_rgn.4.clone();
+                                    aln.extend(r.4);
+                                    last_rgn = (bgn, end, len, orientation, aln);
+                                } else {
+                                    out_rgns.push(last_rgn.clone());
+                                    last_rgn = r.clone();
+                                }
+                            }
+                        });
+                        if last_rgn.2 > 0 {
+                            //not empty
+                            out_rgns.push(last_rgn.clone());
+                        };
+
+                        (sid, out_rgns)
+                    })
+                    .collect::<FxHashMap<_, _>>();
+
+                let ext = format!("{:03}.fa", idx);
+                let mut fasta_out =
+                    BufWriter::new(File::create(prefix.with_extension(ext)).unwrap());
+
+                aln_range.into_iter().for_each(|(sid, rgns)| {
+                    let (ctg, _src, _ctg_len) =
+                        seq_index_db.seq_info.as_ref().unwrap().get(&sid).unwrap();
+                    rgns.into_iter()
+                        .for_each(|(b, e, _, orientation, mut aln)| {
+                            aln.sort();
+                            let q_bgn = aln[0].0 .0;
+                            let q_end = aln[aln.len() - 1].0 .1;
+                            let _ = writeln!(
+                                hit_file,
+                                "{:03}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
+                                idx,
+                                q_name,
+                                ctg,
+                                b,
+                                e,
+                                orientation,
+                                aln.len(),
+                                q_bgn,
+                                q_end
+                            );
+                            let target_seq_name = format!(">{}_{}_{}_{}", ctg, b, e, orientation);
+                            //println!("DBG: {}", seq_id);
+                            let target_seq = seq_index_db
+                                .get_sub_seq_by_id(sid, b as usize, e as usize)
+                                .unwrap();
+                            let target_seq = if orientation == 1 {
+                                pgr_db::fasta_io::reverse_complement(&target_seq)                               
+                            } else {
+                                target_seq
+                            };
+                            let _ = writeln!(fasta_out, "{}", target_seq_name);
+                            let _ = writeln!(fasta_out, "{}", String::from_utf8_lossy(&target_seq));
+                        });
+                });
+            };
+        });
+    Ok(())
+}
