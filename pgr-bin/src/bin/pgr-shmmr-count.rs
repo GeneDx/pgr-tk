6b680d621621b633e0d495f95b2ebc9541e40401
Jason Chin <cschin@infoecho.net>
2023-05-23 17:00:37 +0000

    change the design on how to collect the target shimmers. we get from a fastx file than from the sqeunce database

--- pgr-bin/src/bin/pgr-shmmr-count.rs
+++ pgr-bin/src/bin/pgr-shmmr-count.rs
@@ -1,16 +1,12 @@
 const VERSION_STRING: &str = env!("VERSION_STRING");
 use clap::{self, CommandFactory, Parser};
-use pgr_db::ext::SeqIndexDB;
-use pgr_db::fasta_io;
 use rayon::prelude::*;
 use rustc_hash::FxHashMap;
 use std::fs::File;
-use std::hash::Hash;
-use std::io::{self, BufRead, BufReader, BufWriter, Read, Write};
-use std::path::Path;
+use std::io::{self,  BufReader, BufWriter, Read, Write};
 
 use flate2::bufread::MultiGzDecoder;
-use pgr_db::fasta_io::{FastaReader, FastaStreamReader, FastqStreamReader, SeqRec};
+use pgr_db::fasta_io::{FastaReader, SeqRec};
 enum GZFastaReader {
     GZFile(FastaReader<BufReader<MultiGzDecoder<BufReader<File>>>>),
     RegularFile(FastaReader<BufReader<BufReader<File>>>),
@@ -22,33 +18,20 @@ enum GZFastaReader {
 #[clap(author, version)]
 #[clap(about, long_about = None)]
 struct CmdOptions {
-    /// the prefix to a PGR-TK sequence database
-    pgr_db_prefix: String,
-
-    /// using the frg format for the sequence database (default to the AGC backend database if not specified)
-    #[clap(long, default_value_t = false)]
-    frg_file: bool,
-
-    /// the regions file path
-    #[clap(short, long, default_value=None)]
-    region_file: Option<String>,
+    /// the target fasta file path
+    shmmr_target_fastx: String,
 
     /// ref_fasta
-    #[clap(short, long, default_value=None)]
-    ref_fastx: Option<String>,
+    ref_fastx: String,
 
     /// read_fasta
-    #[clap(short, long, default_value=None)]
-    read_fastx: Option<String>,
+    read_fastx: String,
 
     /// output file name
     #[clap(short, long, default_value=None)]
     output_file: Option<String>,
 
-    /// list all sequence source, contig names in the database
-    #[clap(long, default_value_t = false)]
-    list: bool,
-
+    /// minimizer window size
     #[clap(long, short, default_value_t = 80)]
     w: u32,
     /// minimizer k-mer size
@@ -103,85 +86,45 @@ fn main() -> Result<(), std::io::Error> {
     CmdOptions::command().version(VERSION_STRING).get_matches();
     let args = CmdOptions::parse();
 
-    let mut seq_index_db = SeqIndexDB::new();
-
-    #[cfg(feature = "with_agc")]
-    if args.frg_file {
-        let _ = seq_index_db.load_from_frg_index(args.pgr_db_prefix);
-    } else {
-        let _ = seq_index_db.load_from_agc_index(args.pgr_db_prefix);
-    }
-    #[cfg(not(feature = "with_agc"))]
-    if args.frg_file {
-        let _ = seq_index_db.load_from_frg_index(args.pgr_db_prefix);
-    } else {
-        panic!("This command is compiled with only frg file support, please specify `--frg-file");
-    }
-
-    if args.list {
-        let mut out = if args.output_file.is_some() {
-            let f = File::open(args.output_file.unwrap()).expect("can't open the ouptfile");
-            Box::new(f) as Box<dyn Write>
-        } else {
-            Box::new(io::stdout())
-        };
-        seq_index_db
-            .seq_info
-            .unwrap()
-            .into_iter()
-            .for_each(|(sid, (ctg, src, length))| {
-                writeln!(
-                    out,
-                    "{}\t{}\t{}\t{}",
-                    sid,
-                    src.unwrap_or_else(|| "None".to_string()),
-                    ctg,
-                    length
-                )
-                .expect("can't write output file")
-            });
-        return Ok(());
-    }
-
-    let region_file = args.region_file.expect("region file not specified");
-    let region_file =
-        BufReader::new(File::open(Path::new(&region_file)).expect("can't open the region file"));
-
-    let ref_fastx = args.ref_fastx.expect("ref_fastx file not specified");
-
-    let read_fastx = args.read_fastx.expect("read_fastx file not specified");
-
     let mut shmmr_count = FxHashMap::<u64, (usize, usize)>::default();
 
-    region_file.lines().into_iter().for_each(|line| {
-        let line = line.expect("fail to get a line in the region file");
-        let fields = line.split('\t').collect::<Vec<&str>>();
-        let _label = fields[0].to_string();
-        let src = fields[1].to_string();
-        let ctg = fields[2].to_string();
-        let bgn: usize = fields[3].parse().expect("can't parse bgn");
-        let end: usize = fields[4].parse().expect("can't parse end");
-        let reversed: bool = fields[4].parse::<u32>().expect("can't parse strand") == 1;
-        let mut seq = seq_index_db
-            .get_sub_seq(src, ctg, bgn, end)
-            .expect("fail to fetch sequence");
-        if reversed {
-            seq = fasta_io::reverse_complement(&seq);
-        };
-
-        let shmmrs = pgr_db::shmmrutils::sequence_to_shmmrs1(
-            0,
-            &seq,
-            args.w,
-            args.k,
-            args.r,
-            args.min_span,
-            false,
-        );
-        shmmrs.into_iter().for_each(|mmer| {
-            shmmr_count.insert(mmer.hash(), (0, 0));
+    let mut set_shmmr_count = |seq_iter: &mut dyn Iterator<Item = io::Result<SeqRec>>| {
+        let mut target_seqs: Vec<SeqRec> = vec![];
+        seq_iter.into_iter().for_each(|r| {
+            if let Ok(r) = r {
+                target_seqs.push(r);
+            };
         });
-    });
+        target_seqs
+            .into_par_iter()
+            .map(|seq_rec| {
+                pgr_db::shmmrutils::sequence_to_shmmrs1(
+                    0,
+                    &seq_rec.seq,
+                    args.w,
+                    args.k,
+                    args.r,
+                    args.min_span,
+                    false,
+                )
+                .iter()
+                .map(|&mmer| mmer.hash()).collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>()
+            .into_iter()
+            .for_each(|hashes| hashes.into_iter().for_each(|hash| {
+                shmmr_count.insert(hash, (0,0)); 
+            }));
+    };
+
+    match get_fastx_reader(args.shmmr_target_fastx)? {
+        #[allow(clippy::useless_conversion)] // the into_iter() is necessary for dyn patching
+        GZFastaReader::GZFile(reader) => set_shmmr_count(&mut reader.into_iter()),
+
+        #[allow(clippy::useless_conversion)] // the into_iter() is necessary for dyn patching
+        GZFastaReader::RegularFile(reader) => set_shmmr_count(&mut reader.into_iter()),
+    };
+
 
     let mut ref_seqs: Vec<SeqRec> = vec![];
     let mut ref_shmmr_location = Vec::<(usize, usize, u64)>::new();
@@ -242,7 +185,7 @@ fn main() -> Result<(), std::io::Error> {
             });
     };
 
-    match get_fastx_reader(ref_fastx)? {
+    match get_fastx_reader(args.ref_fastx)? {
         #[allow(clippy::useless_conversion)] // the into_iter() is necessary for dyn patching
         GZFastaReader::GZFile(reader) => count_ref_seq_shmmrs(&mut reader.into_iter()),
 
@@ -295,7 +238,7 @@ fn main() -> Result<(), std::io::Error> {
         });
     };
 
-    match get_fastx_reader(read_fastx)? {
+    match get_fastx_reader(args.read_fastx)? {
         #[allow(clippy::useless_conversion)] // the into_iter() is necessary for dyn patching
         GZFastaReader::GZFile(reader) => count_read_seq_shmmrs(&mut reader.into_iter()),
 
