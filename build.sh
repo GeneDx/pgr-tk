8a88237632b424571e409f78d4541bbd3b32b488
Jason Chin <cschin@infoecho.net>
2023-07-11 14:45:02 -0700

    Update build.sh
    format comments correctly

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@
 
 rustup default stable
 
-# if necessary, you can instal libclang / clang using Anaconda and set LIBCLANG_PATH to point to the libclang for cbindgen dependence clang-sy
-s
+## if necessary, you can instal libclang / clang using Anaconda 
+## and set LIBCLANG_PATH to point to the libclang for cbindgen dependence clang-sys
 # export LIBCLANG_PATH=$HOME/miniconda3/lib
 
-# if necessary, install maturin with `cargo install --locked maturin`
+## if necessary, install maturin with `cargo install --locked maturin`
 # cargo install --locked maturin
 
 cargo build -p pgr-db --release
