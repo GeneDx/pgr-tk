a9e153ea4f9804771508a0d9c76d5dfe3b14d2b2
Jason Chin <cschin@infoecho.net>
2023-07-11 14:42:44 -0700

    Update build.sh
    add comment for build `clang-sys` (needed by `cbingen`) if there is system-wide installation of `libeling`.

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 #make all
 #popd
 
+
+
 rustup default stable
+
+# if necessary, you can instal libclang / clang using Anaconda and set LIBCLANG_PATH to point to the libclang for cbindgen dependence clang-sy
+s
+# export LIBCLANG_PATH=$HOME/miniconda3/lib
+
 cargo build -p pgr-db --release
 cargo build -p pgr-bin --release
 cargo install --path pgr-bin
