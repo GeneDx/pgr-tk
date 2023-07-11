2154cab93416fcd18d9e1e7738d1efd1fd24b853
Jason Chin <cschin@infoecho.net>
2023-07-11 14:44:00 -0700

    Update build.sh
    add comment on installing maturin

--- build.sh
+++ build.sh
@@ -10,6 +10,9 @@ rustup default stable
 s
 # export LIBCLANG_PATH=$HOME/miniconda3/lib
 
+# if necessary, install maturin with `cargo install --locked maturin`
+# cargo install --locked maturin
+
 cargo build -p pgr-db --release
 cargo build -p pgr-bin --release
 cargo install --path pgr-bin
